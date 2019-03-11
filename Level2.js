89d832de2dfc58a4db8ed841b54baa9a01549b22
Jefferson Duong <jeffdeeeee21@gmail.com>
2019-03-11 16:49:39 -0400

    player can move in all directions but when moving right, frame loop ends with dog feet in air

--- Level2.js
+++ Level2.js
@@ -26,15 +26,63 @@ class Level2 extends Phaser.Scene {
 
     player = this.physics.add.sprite(100, 300, 'woof')
     player.setCollideWorldBounds(true)
-    
+
+    this.anims.create ({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('woof', { start: 0, end: 1 }),
+      frameRate: 10,
+      repeat: 0
+  })
+
+    this.anims.create ({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('woof', { start: 2, end: 3 }),
+      frameRate: 10,
+      repeat: 0
+  })
+
+    this.anims.create ({
+      key: 'idleRight',
+      frames: this.anims.generateFrameNumbers('woof', { end: 2 }),
+      frameRate: 10,
+      repeat: -1
+  })
+
+    this.anims.create ({
+      key: 'idleLeft',
+      frames: this.anims.generateFrameNumbers('woof', { end: 1 }),
+      frameRate: 10,
+      repeat: -1
+  })
+
+    this.physics.add.collider(player, platforms)
     //Player controls
 
     cursors = this.input.keyboard.createCursorKeys()
   }
 
   update() {
-    if(cursors.up.isDown) {
-      player.y -=10
+
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160);
+
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160);
+
+        player.anims.play('right', true);
+    }
+    else
+    {
+        player.setVelocityX(0);
+    }
+
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.setVelocityY(-330);
     }
   }
 
