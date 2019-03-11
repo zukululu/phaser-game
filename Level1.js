f4e377bdf5b9445adebe59a4af16d6b9b5e0c534
Jefferson Duong <jeffdeeeee21@gmail.com>
2019-03-11 14:31:20 -0400

    scaled platform asset to make big enough to cover screen. trying to import spritesheet for player to use

--- Level1.js
+++ Level1.js
@@ -5,27 +5,16 @@ class Level1 extends Phaser.Scene {
   }
 
   preload() {
-    // this.load.image('ground', 'assets/platform.png')
   }
 
   create() {
-  this.add.text(77, 112, 'Click to start...', {"font":"bold 20px Arial"});
+  this.add.text(350, 300, 'Click to start...', {"font":"bold 20px Arial"});
   this.input.keyboard.on('keyup_ENTER', function() {
     this.scene.start('Level2')
   }, this)
-
   }
 
   update() {
 
   }
 }
-
-// Level1.prototype.afterCreate = function() {
-//   let startKey = this.input.keyboard.addKey(Phaser.Keyboard.ENTER)
-//   startKey.onDown.add(this.startGame, this)
-// }
-
-// Level1.prototype.startGame = function() {
-//   console.log('hello')
-// }
--- Level2.js
+++ Level2.js
@@ -1,18 +1,32 @@
+let platforms
+let ground
+
 class Level2 extends Phaser.Scene {
   constructor() {
     super({key: 'Level2'})
   }
 
+
   preload() {
     this.load.image('sample', 'assets/sky.png')
+    this.load.image('ground', 'assets/platform.png')
+    this.load.spritesheet('woof', 'assets/woof.png', 32, 32, 4)
   }
 
   create() {
     this.add.image(400, 300, 'sample')
+    console.log('hit enter to go back')
+    this.input.keyboard.on('keyup_ENTER', function() {
+      this.scene.start('Level1')
+    }, this)
+
+    ground = this.add.sprite(400, 500, 'ground')
+    ground.scaleX = 2
+    ground.scaleY = 2
+    console.log(this.scene)
   }
 
   update() {
-
   }
 
 }
