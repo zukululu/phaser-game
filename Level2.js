bece9622717d435b860e37c29431b10bb9359655
Jefferson Duong <jeffdeeeee21@gmail.com>
2019-03-12 10:41:48 -0400

    enemy and enemy firing bullet code blocks getting too large with no results, restarting.

--- Level2.js
+++ Level2.js
@@ -2,54 +2,6 @@ let platforms
 let player
 let cursors
 let moveKeys
-let enemyBullets
-
-let Bullet = new Phaser.Class({
-  Extends: Phaser.GameObjects.Image,
-  initialize:
-
-  function Bullet (scene)
-  {
-    Phaser.GameObjects.Image.call(this, scene, 0, 0, 'bullet')
-    this.speed = 1
-    this.born = 0
-    this.direction = 0
-    this.xSpeed = 0
-    this.ySpeed = 0
-    this.setSize(2, 12, true)
-  },
-
-  fire: function(shooter, target)
-  {
-    this.setPosition(shooter.x, shooter.y)
-    this.direction = Math.atan( (target.x-this.x) / (target.y-this.y))
-
-    if(target.y >= this.y)
-    {
-      this.xSpeed = this.speed*Math.sin(this.direction)
-    }
-    else
-    {
-      this.xSpeed = -this.speed*Math.sin(this.direction);
-      this.ySpeed = -this.speed*Math.cos(this.direction);
-    }
-
-    this.born = 0
-  },
-
-  update: function (time, delta)
-  {
-    this.x += this.xSpeed * delta
-    this.y += this.ySpeed * delta
-    this.born += delta
-    if(this.born > 1800)
-    {
-      this.setActive(false)
-      this.setVisible(false)
-    }
-  }
-}) //End Bullet Class
-
 
 class Level2 extends Phaser.Scene {
   constructor() {
@@ -62,7 +14,9 @@ class Level2 extends Phaser.Scene {
     this.load.image('ground', 'assets/platform.png')
     this.load.image('bullet', 'assets/bullet.png')
     this.load.spritesheet('woof', 'assets/woof.png', { frameWidth: 32, frameHeight: 32 })
+    this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 })
   }
+  
 
   create() {
     this.cameras.main.setBounds(0, 0, 1920 * 2, 1080 * 2)
@@ -73,8 +27,6 @@ class Level2 extends Phaser.Scene {
     this.add.image(0, 1080, 'bg').setOrigin(0).setFlipY(true)
     // this.add.image(1920, 1080, 'bg').setOrigin(0).setFlipX(true).setFlipY(true)
 
-    enemyBullets = this.physics.add.group({ classType: Bullet, runChildUpdate: true})
-
     this.input.keyboard.on('keyup_ENTER', function() {
       this.scene.start('Level1')
     }, this)
@@ -83,9 +35,11 @@ class Level2 extends Phaser.Scene {
     platforms.create(400, 600, 'ground').setScale(2).refreshBody()
     
     player = this.physics.add.sprite(100, 300, 'woof')
+    enemy = this.physics.add.sprite(300, 300, 'dude')
     player.setCollideWorldBounds(true)
     this.physics.add.collider(player, platforms)
-    
+    this.physics.add.collider(enemy, platforms)
+
     this.cameras.main.startFollow(player, true, 0.05, 0.05)
 
   this.anims.create ({
@@ -103,9 +57,9 @@ class Level2 extends Phaser.Scene {
   })
 
   cursors = this.input.keyboard.createCursorKeys()
-  }
+}
 
-  update() {
+  update(time, delta) {
     if (cursors.left.isDown)
     {
         player.setVelocityX(-160);
