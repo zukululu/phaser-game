e6643584bf5185214c7436f0b2731a28d21b7104
Jefferson Duong <jeffdeeeee21@gmail.com>
2019-03-12 14:37:32 -0400

    trying to stick with classes... now fires one bullet on spawn

--- Level2.js
+++ Level2.js
@@ -1,27 +1,31 @@
-let platforms
-let player
-let cursors
-let moveKeys
-let enemy
-var ship
-var lasers
-var mouseTouchDown = false
 
 class Level2 extends Phaser.Scene {
   constructor() {
     super({key: 'Level2'})
+  this.platforms
+  this.player
+  this.cursors
+  this.moveKeys
+  this.enemy
+  this.bullet
   }
 
   preload() {
     this.load.image('bg', 'assets/the-end-by-iloe-and-made.jpg')
     this.load.image('sample', 'assets/sky.png')
     this.load.image('ground', 'assets/platform.png')
-    this.load.image('laser', 'assets/bullet.png')
+    this.load.image('bullet', 'assets/bullet.png')
     this.load.spritesheet('woof', 'assets/woof.png', { frameWidth: 32, frameHeight: 32 })
     this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 })
   }
   
   create() {
+    //Sample scene transition
+    this.input.keyboard.on('keyup_ENTER', function() {
+      this.scene.start('Level1')
+    }, this)
+    
+    //World Creation
     this.cameras.main.setBounds(0, 0, 1920 * 2, 1080 * 2)
     this.physics.world.setBounds(0, 0, 1920 * 2, 1080 * 2)
 
@@ -30,20 +34,25 @@ class Level2 extends Phaser.Scene {
     this.add.image(0, 1080, 'bg').setOrigin(0).setFlipY(true)
     // this.add.image(1920, 1080, 'bg').setOrigin(0).setFlipX(true).setFlipY(true)
 
-    this.input.keyboard.on('keyup_ENTER', function() {
-      this.scene.start('Level1')
-    }, this)
+    //Bullets
+    this.bullet = this.physics.add.image(200, 290, 'bullet')
+    this.physics.add.overlap(this.bullet, this.enemy, this.bulletHit, null, this)
     
-    platforms = this.physics.add.staticGroup()
-    platforms.create(400, 600, 'ground').setScale(2).refreshBody()
+    //Dummy Platform
+    this.platforms = this.physics.add.staticGroup()
+    this.platforms.create(400, 600, 'ground').setScale(2).refreshBody()
     
-    player = this.physics.add.sprite(100, 300, 'woof')
-    enemy = this.physics.add.sprite(300, 300, 'dude')
-    player.setCollideWorldBounds(true)
-    this.physics.add.collider(player, platforms)
-    this.physics.add.collider(enemy, platforms)
+    //Player creation
+    this.player = this.physics.add.sprite(100, 300, 'woof')
+    this.enemy = this.physics.add.sprite(300, 300, 'dude')
+    this.player.setCollideWorldBounds(true)
+    this.physics.add.collider(this.player, this.platforms)
+    this.physics.add.collider(this.enemy, this.platforms)
 
-    this.cameras.main.startFollow(player, true, 0.05, 0.05)
+    //Camera
+    this.cameras.main.startFollow(this.player, true, 0.05, 0.05)
+    
+    this.launchBullet()
 
   this.anims.create ({
     key: 'left',
@@ -59,32 +68,44 @@ class Level2 extends Phaser.Scene {
     repeat: 0
   })
 
-  cursors = this.input.keyboard.createCursorKeys()
+  this.cursors = this.input.keyboard.createCursorKeys()
 }
 
+  bulletHit( bullet, enemy) {
+    this.launchBullet()
+    enemy.body.enable = false
+    this.enemy.killAndHide(enemy)
+  }
+
+  launchBullet() {
+    console.log(this.player)
+    this.bullet.body.reset(this.player.x, this.player.y)
+    this.bullet.body.velocity.x = 400
+  }
+
   update() {
-    if (cursors.left.isDown)
+    if (this.cursors.left.isDown)
     {
-        player.setVelocityX(-160);
+        this.player.setVelocityX(-160);
 
-        player.anims.play('left', true);
-        player.flipX = false
+        this.player.anims.play('left', true);
+        this.player.flipX = false
     }
-    else if (cursors.right.isDown)
+    else if (this.cursors.right.isDown)
     {
-        player.setVelocityX(160);
+        this.player.setVelocityX(160);
 
-        player.anims.play('left', true);
-        player.flipX = true
+        this.player.anims.play('left', true);
+        this.player.flipX = true
     }
     else
     {
-        player.setVelocityX(0);
+        this.player.setVelocityX(0);
     }
 
-    if (cursors.up.isDown && player.body.touching.down)
+    if (this.cursors.up.isDown && this.player.body.touching.down)
     {
-        player.setVelocityY(-330);
+        this.player.setVelocityY(-330);
     }
   }
 }
--- Player.js
+++ Player.js
@@ -1,21 +1,42 @@
-let player
-let cursors
 
-class Player extends Phaser.Scene {
+class Level2 extends Phaser.Scene {
   constructor() {
     super({key: 'Player'})
+  this.player
+  this.cursors
+  this.moveKeys
   }
 
   preload() {
+    this.load.image('bg', 'assets/the-end-by-iloe-and-made.jpg')
+    this.load.image('sample', 'assets/sky.png')
+    this.load.image('ground', 'assets/platform.png')
     this.load.spritesheet('woof', 'assets/woof.png', { frameWidth: 32, frameHeight: 32 })
   }
-
+  
   create() {
-    player = this.physics.add.sprite(100, 300, 'woof')
-    player.setCollideWorldBounds(true)
-    this.physics.add.collider(player)
-    // console.log(player)
+    this.player = this.physics.add.sprite(100, 300, 'woof')
+    this.cameras.main.setBounds(0, 0, 1920 * 2, 1080 * 2)
+    this.physics.world.setBounds(0, 0, 1920 * 2, 1080 * 2)
 
+    this.add.image(0, 0, 'bg').setOrigin(0)
+    // this.add.image(1920, 0, 'bg').setOrigin(0).setFlipX(true)
+    this.add.image(0, 1080, 'bg').setOrigin(0).setFlipY(true)
+    // this.add.image(1920, 1080, 'bg').setOrigin(0).setFlipX(true).setFlipY(true)
+
+    this.input.keyboard.on('keyup_ENTER', function() {
+      this.scene.start('Level1')
+    }, this)
+    
+    this.platforms = this.physics.add.staticGroup()
+    this.platforms.create(400, 600, 'ground').setScale(2).refreshBody()
+    
+    // this.enemy = this.physics.add.sprite(300, 300, 'dude')
+    this.player.setCollideWorldBounds(true)
+    this.physics.add.collider(this.player, this.platforms)
+    this.physics.add.collider(this.enemy, this.platforms)
+
+    this.cameras.main.startFollow(this.player, true, 0.05, 0.05)
 
   this.anims.create ({
     key: 'left',
@@ -31,31 +52,32 @@ class Player extends Phaser.Scene {
     repeat: 0
   })
 
-  cursors = this.input.keyboard.createCursorKeys()
-  }
+  this.cursors = this.input.keyboard.createCursorKeys()
+}
 
   update() {
-
-    if (cursors.left.isDown)
+    if (this.cursors.left.isDown)
     {
-        player.setVelocityX(-160);
+        this.player.setVelocityX(-160);
 
-        player.anims.play('left', true);
+        this.player.anims.play('left', true);
+        this.player.flipX = false
     }
-    else if (cursors.right.isDown)
+    else if (this.cursors.right.isDown)
     {
-        player.setVelocityX(160);
+        this.player.setVelocityX(160);
 
-        player.anims.play('right', true);
+        this.player.anims.play('left', true);
+        this.player.flipX = true
     }
     else
     {
-        player.setVelocityX(0);
+        this.player.setVelocityX(0);
     }
 
-    if (cursors.up.isDown && player.body.touching.down)
+    if (this.cursors.up.isDown && this.player.body.touching.down)
     {
-        player.setVelocityY(-330);
+        this.player.setVelocityY(-330);
     }
   }
 }
