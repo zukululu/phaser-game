1567eddfd04d34af33d662dd3157c06ceeb65af1
Jefferson Duong <jeffdeeeee21@gmail.com>
2019-03-13 15:03:36 -0400

    i believe i have everything necessary to make the enemy fire at the enemy and track the players movements, i just basically need to recreate players launchBullet function for the flyingEnemy. also need to add bullets to destroy on impact with platforms, which are collideable terrain

--- Level2.js
+++ Level2.js
@@ -10,7 +10,63 @@ class Level2 extends Phaser.Scene {
   this.bullets
   this.space
   this.facing = 'left'
+  this.enemyBullets
   }
+
+    Bullet = new Phaser.Class({
+
+      Extends: Phaser.GameObjects.Image,
+
+      initialize:
+
+      // Bullet Constructor
+      function Bullet (scene)
+      {
+          Phaser.GameObjects.Image.call(this, scene, 0, 0, 'bullet');
+          this.speed = 1;
+          this.born = 0;
+          this.direction = 0;
+          this.xSpeed = 0;
+          this.ySpeed = 0;
+          this.setSize(12, 12, true);
+      },
+
+      // Fires a bullet from the player to the reticle
+      fire: function (shooter, target)
+      {
+          this.setPosition(shooter.x, shooter.y); // Initial position
+          this.direction = Math.atan( (target.x-this.x) / (target.y-this.y));
+
+          // Calculate X and y velocity of bullet to moves it from shooter to target
+          if (target.y >= this.y)
+          {
+              this.xSpeed = this.speed*Math.sin(this.direction);
+              this.ySpeed = this.speed*Math.cos(this.direction);
+          }
+          else
+          {
+              this.xSpeed = -this.speed*Math.sin(this.direction);
+              this.ySpeed = -this.speed*Math.cos(this.direction);
+          }
+
+          this.rotation = shooter.rotation; // angle bullet with shooters rotation
+          this.born = 0; // Time since new bullet spawned
+      },
+
+      // Updates the position of the bullet each cycle
+      update: function (time, delta)
+      {
+          this.x += this.xSpeed * delta;
+          this.y += this.ySpeed * delta;
+          this.born += delta;
+          if (this.born > 1800)
+          {
+              this.setActive(false);
+              this.setVisible(false);
+          }
+      }
+
+  });
 
   preload() {
     console.log('preload')
@@ -31,9 +87,12 @@ class Level2 extends Phaser.Scene {
     
     //World Creation
     console.log(this)
-    console.log(this.cameras.cameras[0].displayHeight)
     this.cameras.main.setBounds(0, 0, 1920 * 2, 1080 * 2)
-    this.physics.world.setBounds(0, 0, 1920, 1080)
+    // this.physics.world.setBounds(0, 0, 1920, 1080)
+
+    //Bullet Group for objects for enemy
+    this.enemyBullets = this.physics.add.group({ classType: this.Bullet, runChildUpdate: true })
+    console.log(this.enemyBullets)
 
     this.add.image(0, 0, 'bg').setOrigin(0)
     // this.add.image(1920, 0, 'bg').setOrigin(0).setFlipX(true)
@@ -46,11 +105,17 @@ class Level2 extends Phaser.Scene {
     
     //Player creation
     this.player = this.physics.add.sprite(100, 300, 'woof')
-    this.enemy = this.physics.add.sprite(300, 300, 'dude')
     this.player.setCollideWorldBounds(true)
+    
+    this.enemy = this.physics.add.sprite(300, 300, 'dude')
+    this.flyingEnemy = this.physics.add.sprite(300, 300, 'dude')
+    this.flyingEnemy.body.allowGravity = false
+    this.flyingEnemy.lastFired = 0
+    console.log(this.flyingEnemy)
+    
     this.physics.add.collider(this.player, this.platforms)
     this.physics.add.collider(this.enemy, this.platforms)
-
+    
     //Enemy Collision
     this.physics.add.overlap(this.player, this.enemy, this.enemyCollision, null, this)
 
@@ -76,11 +141,14 @@ class Level2 extends Phaser.Scene {
   }
 
   enemyCollision() {
-    this.player.setActive(false).setVisible(false)
+    if(this.enemy.active === true)
+      this.player.setActive(false).setVisible(false)
   }
 
   launchBullet() {
       //Create and initialize bullet properties
+
+      console.log(this.time)
       this.bullet = this.physics.add.sprite(this.player.x, this.player.y, 'bullet')
       this.physics.add.overlap(this.bullet, this.enemy, this.bulletHit, null, this)
       this.bullet.body.allowGravity = false
@@ -101,10 +169,9 @@ class Level2 extends Phaser.Scene {
 }
 
   bulletHit(bullet, enemy) {
+    this.physics.world.removeCollider(this.enemy);
     this.enemy.setActive(false).setVisible(false)
     this.bullet.destroy()
-    console.log('dead?')
-    console.log(this.enemy)
   }
 
   enemyChase() {
@@ -113,12 +180,34 @@ class Level2 extends Phaser.Scene {
     // if(this.enemy.x)
     // console.log('exists')
     if(this.enemy.x < this.player.x) {
-      this.enemy.setVelocityX(80)
+      this.enemy.setVelocityX(50)
     } else if (this.enemy.x > this.player.x) {
-      this.enemy.setVelocityX(-80)
+      this.enemy.setVelocityX(-50)
     } else {
       return
     }
+  }
+
+  enemyFire(enemy, player, time, gameObject) {
+      if (enemy.active === false)
+      {
+          return;
+      }
+
+      if ((time - enemy.lastFired) > 1000)
+      {
+          enemy.lastFired = time;
+
+          // Get bullet from bullets group
+          var bullet = enemyBullets.get().setActive(true).setVisible(true);
+
+          if (bullet)
+          {
+              bullet.fire(enemy, player);
+              // Add collider between bullet and player
+              gameObject.physics.add.collider(player, bullet, playerHitCallback);
+          }
+      }
   }
 
   update() {
@@ -143,16 +232,17 @@ class Level2 extends Phaser.Scene {
 
     if (this.cursors.up.isDown && this.player.body.touching.down)
     {
-        this.player.setVelocityY(-330);
+        this.player.setVelocityY(-370);
     }
 
     if(Phaser.Input.Keyboard.JustDown(this.space)) {
         this.launchBullet()
     }
 
-    console.log('updating...')
-      if(this.player.x <= this.enemy.x + 500 || this.player.x <= this.enemy.x - 500 && this.player.y === this.enemy.y) {
+    if(this.player.x <= this.enemy.x + 500 || this.player.x <= this.enemy.x - 500 && this.player.y === this.enemy.y && this.enemy.active !== false) {
         this.enemyChase()
     }
+
+    this.enemyFire(this.flyingEnemy, this.player, this.time, this);
   }
 }
